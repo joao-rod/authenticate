d7a7300f220e468a0f6f763efa6caa62ec58d9c7
Joao Victor <jvr2564@gmail.com>
2021-11-24 16:15:57 -0300

    Criação da UI

---
+++ generate_cod.py
@@ -0,0 +1,6 @@
+from random import randint
+
+def get_cod():
+  cod = randint(100000, 999999)
+  
+  return cod
--- main.py
+++ main.py
@@ -1,10 +1,91 @@
-from random import randint
+import PySimpleGUI as sg
+
+from generate_cod import get_cod
 from message import send_message
 
-cod = randint(100000, 999999)
-send_message(cod)
-num = int(input('Digite o código recebido por SMS.'))
-if num == cod:
-  print('Autenticado com sucesso.')
-else:
-  print('Ocorreu um erro...')
+validator = get_cod()
+send_message(validator)
+
+layout = [
+  [
+    sg.Text(
+      'Digite o código de 6 dígitos recebido por SMS',
+      background_color='#1D1C2D',
+      text_color='#e3e3e3',
+      font='"Roboto" 14', 
+      size=(32, 0)
+    )
+  ],
+  
+  [
+    sg.Input(
+      key='code',
+      background_color='#1D1C19',
+      text_color='#e3e3e3',
+      font='"Roboto" 14', 
+      size=(32, 2)
+    )
+  ],
+  
+  [
+    sg.Text(
+      key='space',
+      background_color='#1D1C2D',
+      size=(0, 1)
+    )
+  ],
+  
+  [
+    sg.Button(
+      'Submeter',
+      key='ok',
+      button_color='#1D1C20',
+      font='"Roboto" 14', 
+      size=(10, 0)
+    ),
+    
+    sg.Text(
+      key='space',
+      background_color='#1D1C2D',
+      size=(8, 0)
+    ),
+    
+     sg.Button(
+      'Cancelar',
+      key='error',
+      button_color='#1D1C20',
+      font='"Roboto" 14', 
+      size=(10, 0)
+    )
+  ],
+  
+  [
+      sg.Text(
+      key='response',
+      background_color='#1D1C2D',
+      text_color='#e3e3e3',
+      font='"Roboto" 14', 
+      size=(32, 0)
+    )
+  ]
+]
+
+window = sg.Window(
+  'Autentique-se',
+  layout,
+  background_color='#1D1C2D',
+  size=(410, 300),
+  location=(500, 150)
+)
+
+while True:
+  events, values = window.read()
+  if events == sg.WINDOW_CLOSED or events == 'error':
+    break
+  
+  if events == 'ok':
+    if str(validator) == values['code']:
+      window['response'].update('Autenticado com sucesso.', text_color='#50fa7b')
+      window.refresh()
+    else:
+      window['response'].update('Código incorreto. Tente novamente.', text_color='#ff5555')
